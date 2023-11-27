841404ffd4f5841f335758b72197046290624312
Daniel Promise <promise.olayimika.171076@fuoye.edu.ng>
2023-11-27 20:24:04 +0100

    fix:changed file format

--- services/ffmpeg.py
+++ services/ffmpeg.py
@@ -3,14 +3,14 @@ import uuid
 
 def process_audio(audio_blob, user_id):
   # Create unique filenames
-  input_filename = f"{user_id}_{uuid.uuid4()}.webm"
-  output_filename = f"{user_id}_{uuid.uuid4()}.webm"
+  input_filename = f"{user_id}_{uuid.uuid4()}"
+  output_filename = f"{user_id}_{uuid.uuid4()}.wav"
 
   # Write the audio blob to a file
   with open(input_filename, 'wb') as file:
     file.write(audio_blob)
 
   # Re-encode audio using ffmpeg
-  ffmpeg.input(input_filename).output(output_filename).run(loglevel='info')
+  ffmpeg.input(input_filename).output(output_filename).run()
 
   return input_filename, output_filename
